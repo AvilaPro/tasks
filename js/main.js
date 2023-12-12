c0f161d09d5cc0bc996f4fcd9cbdf4ef2138e46e
avilapro <mejiasavilaeduardo@gmail.com>
2023-12-12 11:37:22 -0400

    v2. Ajustes para editar y elimnar items de la lista. Correccion del gesto del doble click que no funcionaba en moviles.

--- js/main.js
+++ js/main.js
@@ -28,11 +28,18 @@ function ejecucionInicial() {
         // Crear el elemento li
         const elementoLi = document.createElement("li");
 
-        // Agregar el contenido al elemento li
-        elementoLi.textContent = elemento;
-
         // Agregar la clase list-group-item al elemento li
         elementoLi.classList.add("list-group-item");
+
+        //creamos el span y le agregamos el contenido
+        let spanLi = document.createElement('span');
+        spanLi.textContent = elemento;
+
+        elementoLi.appendChild(spanLi);
+
+        //agregamos manejador de evento del doble click para eliminar tanto de la lista como del localStorage
+        elementoLi.setAttribute('ondblclick', "if (confirm('deseas eliminar esta tarea?')) { console.log(this.textContent); eliminarElementoDeArregloEnLocalStorage('arreglo', this.textContent); this.parentNode.removeChild(this);}");
+        elementoLi.innerHTML += '<button class="btn btn-warning" onclick="editarLi(this)"><i class="bi bi-pen"></i></button> <button class="btn btn-danger" onclick="eliminarLi(this)"><i class="bi bi-x-circle"></i></button>'
 
         // Agregar el elemento li al elemento ul
         elementoUl.appendChild(elementoLi);
@@ -60,6 +67,27 @@ function agregarArregloAlLocalStorage(arreglo) {
   // Almacenar el arreglo en el localStorage
   localStorage.setItem(clave, arregloEnJSON);
 }
+
+function editarElementoArregloEnLocalStorage(clave, elementoOriginal, elementoEditado) {
+  // Obtener el arreglo del localStorage
+  const arregloEnJSON = localStorage.getItem(clave);
+
+  // Convertir el arreglo JSON a un arreglo
+  const arreglo = JSON.parse(arregloEnJSON);
+
+  // Buscar el elemento en el arreglo
+  const indice = arreglo.indexOf(elementoOriginal);
+
+  // Si se encontró el elemento, editarlo
+  if (indice !== -1) {
+    arreglo.splice(indice, 1, elementoEditado);
+
+    // Actualizar el arreglo en el localStorage
+    const arregloEnJSONActualizado = JSON.stringify(arreglo);
+    localStorage.setItem(clave, arregloEnJSONActualizado);
+  }
+}
+
 
 function eliminarElementoDeArregloEnLocalStorage(clave, elemento) {
   // Obtener el arreglo del localStorage
@@ -93,17 +121,42 @@ function agregarTarea() {
   //creamos un elemento li
   let li = document.createElement('li');
   //agregamos el texto del input al elemento li
+  let spanLi = document.createElement('span');
   let contentLi = document.createTextNode(producto);
   //agregar clase de item lista de bootstrap
   li.setAttribute('class', 'list-group-item');
   //agregamos manejador de evento del doble click para eliminar tanto de la lista como del localStorage
-  li.setAttribute('ondblclick', "if (confirm('deseas eliminar esta tarea?')) { console.log(this.textContent); eliminarElementoDeArregloEnLocalStorage('arreglo', this.textContent); this.parentNode.removeChild(this);}");
+  // li.setAttribute('ondblclick', "if (confirm('deseas eliminar esta tarea?')) { console.log(this); eliminarElementoDeArregloEnLocalStorage('arreglo', this.textContent); this.parentNode.removeChild(this);}");
   //agregamos contenido al li
-  li.appendChild(contentLi);
+  spanLi.appendChild(contentLi);
+  li.appendChild(spanLi);
+  li.innerHTML += '<button class="btn btn-warning" onclick="editarLi(this)"><i class="bi bi-pen"></i></button> <button class="btn btn-danger" onclick="eliminarLi(this)"><i class="bi bi-x-circle"></i></button>'
   //agregamos el li a la lista
   elePadre.appendChild(li);
   //Limpiar el input
   document.getElementById("req").value = "";
 
 }
+
+function editarLi(el) {
+  console.log(el.parentNode);
+  if (confirm('Deseas editarlo?')) {
+    nuevoValor = prompt(`Ingresa el nuevo valor: (antes: ${el.parentNode.children[0].textContent})`);
+    if (nuevoValor.length > 0) {
+      editarElementoArregloEnLocalStorage('arreglo', el.parentNode.children[0].textContent, nuevoValor);
+      el.parentNode.parentNode.innerHTML = '';
+      ejecucionInicial();
+    }
+  }
+}
+
+function eliminarLi(el) {
+  if (confirm('Deseas eliminarlo?')) {
+    eliminarElementoDeArregloEnLocalStorage('arreglo', el.parentNode.children[0].textContent);
+    el.parentNode.parentNode.innerHTML = '';
+    ejecucionInicial();
+  }
+}
+
+
 
